vsergeev/c-periphery

commit 56755acc7847d6096e824d0a8270f820176be320

spi: add advanced open function with 32-bit extra flags

src/spi.c
@@ -68,6 +68,11 @@ int spi_open(spi_t *spi, const char *path, unsigned int mode, uint32_t max_speed
 }
 
 int spi_open_advanced(spi_t *spi, const char *path, unsigned int mode, uint32_t max_speed, spi_bit_order_t bit_order, uint8_t bits_per_word, uint8_t extra_flags) {
+    return spi_open_advanced2(spi, path, mode, max_speed, bit_order, bits_per_word, extra_flags);
+}
+
+int spi_open_advanced2(spi_t *spi, const char *path, unsigned int mode, uint32_t max_speed, spi_bit_order_t bit_order, uint8_t bits_per_word, uint32_t extra_flags) {
+    uint32_t data32;
     uint8_t data8;
 
     /* Validate arguments */
@@ -75,6 +80,10 @@ int spi_open_advanced(spi_t *spi, const char *path, unsigned int mode, uint32_t 
         return _spi_error(spi, SPI_ERROR_ARG, 0, "Invalid mode (can be 0,1,2,3)");
     if (bit_order != MSB_FIRST && bit_order != LSB_FIRST)
         return _spi_error(spi, SPI_ERROR_ARG, 0, "Invalid bit order (can be MSB_FIRST,LSB_FIRST)");
+#ifndef SPI_IOC_WR_MODE32
+    if (extra_flags > 0xff)
+        return _spi_error(spi, SPI_ERROR_UNSUPPORTED, "Kernel version does not support 32-bit SPI mode flags");
+#endif
 
     memset(spi, 0, sizeof(spi_t));
 
@@ -83,6 +92,7 @@ int spi_open_advanced(spi_t *spi, const char *path, unsigned int mode, uint32_t 
         return _spi_error(spi, SPI_ERROR_OPEN, errno, "Opening SPI device \"%s\"", path);
 
     /* Set mode, bit order, extra flags */
+#ifndef SPI_IOC_WR_MODE32
     data8 = mode | ((bit_order == LSB_FIRST) ? SPI_LSB_FIRST : 0) | extra_flags;
     if (ioctl(spi->fd, SPI_IOC_WR_MODE, &data8) < 0) {
         int errsv = errno;
@@ -90,6 +100,28 @@ int spi_open_advanced(spi_t *spi, const char *path, unsigned int mode, uint32_t 
         spi->fd = -1;
         return _spi_error(spi, SPI_ERROR_CONFIGURE, errsv, "Setting SPI mode");
     }
+#else
+    if (extra_flags > 0xff) {
+        /* Use 32-bit mode if extra_flags is wider than 8-bits */
+        data32 = mode | ((bit_order == LSB_FIRST) ? SPI_LSB_FIRST : 0) | extra_flags;
+        if (ioctl(spi->fd, SPI_IOC_WR_MODE32, &data32) < 0) {
+            int errsv = errno;
+            close(spi->fd);
+            spi->fd = -1;
+            return _spi_error(spi, SPI_ERROR_CONFIGURE, errsv, "Setting SPI mode");
+        }
+    } else {
+        /* Prefer 8-bit mode, in case this library is inadvertently used on an
+         * older kernel. */
+        data8 = mode | ((bit_order == LSB_FIRST) ? SPI_LSB_FIRST : 0) | extra_flags;
+        if (ioctl(spi->fd, SPI_IOC_WR_MODE, &data8) < 0) {
+            int errsv = errno;
+            close(spi->fd);
+            spi->fd = -1;
+            return _spi_error(spi, SPI_ERROR_CONFIGURE, errsv, "Setting SPI mode");
+        }
+    }
+#endif
 
     /* Set max speed */
     if (ioctl(spi->fd, SPI_IOC_WR_MAX_SPEED_HZ, &max_speed) < 0) {

src/spi.h
@@ -38,6 +38,9 @@ int spi_open(spi_t *spi, const char *path, unsigned int mode,
 int spi_open_advanced(spi_t *spi, const char *path, unsigned int mode,
                         uint32_t max_speed, spi_bit_order_t bit_order,
                         uint8_t bits_per_word, uint8_t extra_flags);
+int spi_open_advanced2(spi_t *spi, const char *path, unsigned int mode,
+                       uint32_t max_speed, spi_bit_order_t bit_order,
+                       uint8_t bits_per_word, uint32_t extra_flags);
 int spi_transfer(spi_t *spi, const uint8_t *txbuf, uint8_t *rxbuf, size_t len);
 int spi_close(spi_t *spi);
 void spi_free(spi_t *spi);

commit 708f7fe239d9de83bcad46304f6d65f6f0adf159

spi: fix calls to error helper when building without 32-bit mode support
Commit 56755acc7847d6096e824d0a8270f820176be320 introduced support for
32-bit extra flags. However, when compiling with a toolchain without
support for the extra flags, the following error appears:

  src/spi.c:322:5: error: too few arguments to function '_spi_error'
     return _spi_error(spi, SPI_ERROR_UNSUPPORTED, "Kernel version does not support 32-bit SPI mode flags");

This can be fixed by passing a third argument of 0.

Signed-off-by: Ryan Barnett <ryanbarnett3@gmail.com>
Signed-off-by: Vanya A. Sergeev <v@sergeev.io>

src/spi.c
@@ -82,7 +82,7 @@ int spi_open_advanced2(spi_t *spi, const char *path, unsigned int mode, uint32_t
         return _spi_error(spi, SPI_ERROR_ARG, 0, "Invalid bit order (can be MSB_FIRST,LSB_FIRST)");
 #ifndef SPI_IOC_WR_MODE32
     if (extra_flags > 0xff)
-        return _spi_error(spi, SPI_ERROR_UNSUPPORTED, "Kernel version does not support 32-bit SPI mode flags");
+        return _spi_error(spi, SPI_ERROR_UNSUPPORTED, 0, "Kernel version does not support 32-bit SPI mode flags");
 #endif
 
     memset(spi, 0, sizeof(spi_t));
@@ -246,7 +246,7 @@ int spi_get_extra_flags32(spi_t *spi, uint32_t *extra_flags) {
 
     return 0;
 #else
-    return _spi_error(spi, SPI_ERROR_UNSUPPORTED, "Kernel version does not support 32-bit SPI mode flags");
+    return _spi_error(spi, SPI_ERROR_UNSUPPORTED, 0, "Kernel version does not support 32-bit SPI mode flags");
 #endif
 }
 
@@ -319,7 +319,7 @@ int spi_set_extra_flags32(spi_t *spi, uint32_t extra_flags) {
 
     return 0;
 #else
-    return _spi_error(spi, SPI_ERROR_UNSUPPORTED, "Kernel version does not support 32-bit SPI mode flags");
+    return _spi_error(spi, SPI_ERROR_UNSUPPORTED, 0, "Kernel version does not support 32-bit SPI mode flags");
 #endif
 }
 