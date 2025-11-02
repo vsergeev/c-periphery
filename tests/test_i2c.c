a57b00ade199bab6ddbe1132d370e3f2d7c587de
Vanya A. Sergeev <v@sergeev.io>
2025-11-01 19:45:50 -0500

    tests/i2c: add eeprom write/read to loopback test

--- tests/test_i2c.c
+++ tests/test_i2c.c
@@ -17,6 +17,8 @@
 
 #include "../src/i2c.h"
 
+#define I2C_EEPROM_ADDRESS      0x51
+
 const char *i2c_bus_path;
 
 void test_arguments(void) {
@@ -46,9 +48,63 @@ void test_open_config_close(void) {
 }
 
 void test_loopback(void) {
+    i2c_t *i2c;
+    uint8_t vector[32];
+    uint8_t buf[2 + 32];
+    unsigned int i;
+    struct i2c_msg msgs[2];
+
     ptest();
 
-    printf("No general way to do a loopback test for I2C without a real component, skipping...\n");
+    /* Allocate I2C */
+    i2c = i2c_new();
+    passert(i2c != NULL);
+
+    passert(i2c_open(i2c, i2c_bus_path) == 0);
+
+    /* Generate random byte vector */
+    srandom(1234);
+    for (i = 0; i < sizeof(vector); i++) {
+        vector[i] = (uint8_t)random();
+    }
+
+    /* Write bytes to 0x100 */
+    /* S [ 0x51 W ] [ 0x01 ] [ 0x00 ] [ Data... ] P */
+    buf[0] = 0x01;
+    buf[1] = 0x00;
+    memcpy(buf + 2, vector, sizeof(vector));
+    msgs[0].addr = I2C_EEPROM_ADDRESS;
+    msgs[0].flags = 0; /* Write */
+    msgs[0].len = sizeof(buf);
+    msgs[0].buf = buf;
+    passert(i2c_transfer(i2c, msgs, 1) == 0);
+
+    /* Wait for Write Cycle */
+    usleep(10000);
+
+    /* Read bytes from 0x100 */
+    /* S [ 0x51 W ] [ 0x01 ] [ 0x00 ] S [ 0x51 R ] [ Data... ] P */
+    buf[0] = 0x01;
+    buf[1] = 0x00;
+    memset(buf + 2, 0, sizeof(vector));
+    msgs[0].addr = I2C_EEPROM_ADDRESS;
+    msgs[0].flags = 0; /* Write */
+    msgs[0].len = 2;
+    msgs[0].buf = buf;
+    msgs[1].addr = I2C_EEPROM_ADDRESS;
+    msgs[1].flags = I2C_M_RD; /* Read */
+    msgs[1].len = sizeof(vector);
+    msgs[1].buf = buf + 2;
+    passert(i2c_transfer(i2c, msgs, 2) == 0);
+
+    /* Verify bytes */
+    passert(memcmp(buf + 2, vector, sizeof(vector)) == 0);
+
+    /* Close I2C */
+    passert(i2c_close(i2c) == 0);
+
+    /* Free I2C */
+    i2c_free(i2c);
 }
 
 bool getc_yes(void) {
@@ -109,7 +165,7 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "Usage: %s <I2C device>\n\n", argv[0]);
         fprintf(stderr, "[1/4] Arguments test: No requirements.\n");
         fprintf(stderr, "[2/4] Open/close test: I2C device should be real.\n");
-        fprintf(stderr, "[3/4] Loopback test: No test.\n");
+        fprintf(stderr, "[3/4] Loopback test: Expects 24XX32 EEPROM (or similar) at address 0x51.\n");
         fprintf(stderr, "[4/4] Interactive test: I2C bus should be observed with an oscilloscope or logic analyzer.\n\n");
         fprintf(stderr, "Hint: for Raspberry Pi 3, enable I2C1 with:\n");
         fprintf(stderr, "   $ echo \"dtparam=i2c_arm=on\" | sudo tee -a /boot/config.txt\n");
